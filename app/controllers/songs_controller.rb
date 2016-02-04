f6b6178b68b4c27dfd38473a3153a3c8a4bc559a
Yuta Goto <Yuta Goto>
2016-02-05 00:26:59 +0900

    add songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,33 @@
 class SongsController < ApplicationController
+
+	def new
+		@song = Song.new
+	end
+
+	def create
+		@song = Song.new(song_params)
+	end
+
+	def index
+		@songs = Song.all
+	end
+
+	def show
+		@song = Song.find(params[:id])
+	end
+
+	def update
+		@song = Song.find(params[:id])
+	end
+
+	def destloy
+		@song = Song.find(params[:id])
+	end
+
+	private
+
+	def song_params
+		
+	end
+
 end
