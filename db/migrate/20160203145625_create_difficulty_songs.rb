fa936b9f2282a2d56c85e2a07266fafbaae25097
Yuta Goto <Yuta Goto>
2016-02-13 21:32:24 +0900

    star

--- db/migrate/20160203145625_create_difficulty_songs.rb
+++ db/migrate/20160203145625_create_difficulty_songs.rb
@@ -3,7 +3,7 @@ class CreateDifficultySongs < ActiveRecord::Migration
     create_table :difficulty_songs do |t|
       t.references  :song, index: true, null: false
       t.references  :difficulty, index: true, null: false
-      t.integer     :ster
+      t.integer     :star
 
       t.timestamps null: false
     end
