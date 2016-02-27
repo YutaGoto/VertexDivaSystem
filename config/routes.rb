f0a280f18204ba31614d460bdf96b575e0f0e303
YutaGoto <you.goto.510@gmail.com>
2016-02-27 19:21:31 +0900

    welcome thanks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  get :thanks, controller: :welcome, action: :thanks
+
   namespace :backend do
     resources :songs
     resources :vocalists
--- db/migrate/20160203144900_create_songs.rb
+++ db/migrate/20160203144900_create_songs.rb
@@ -2,6 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string      'title'
+      t.string      'title_kana'
       t.datetime    'release_date'
 
       t.timestamps  null: false
