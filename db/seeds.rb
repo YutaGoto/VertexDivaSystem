eb27c898925731c4bfd56bc5dfe3cde3d17427fe
YutaGoto <goto@rejob.co.jp>
2016-03-12 21:54:33 +0900

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,3 @@
-# マスターデータ
-Difficulty.destroy_all
-Difficulty.create(
-  [
-    { name: 'Easy' },
-    { name: 'Normal' },
-    { name: 'Hard' },
-    { name: 'Extreme' },
-    { name: 'ExExtreme' }
-  ]
-)
-
 # admin
 Admin.destroy_all
 Admin.create(
@@ -20,6 +8,18 @@ Admin.create(
       password: 'test1234',
       twitter_id: 'gggooottto'
     }
+  ]
+)
+
+# マスターデータ
+Difficulty.destroy_all
+Difficulty.create(
+  [
+    { name: 'Easy' },
+    { name: 'Normal' },
+    { name: 'Hard' },
+    { name: 'Extreme' },
+    { name: 'ExExtreme' }
   ]
 )
 
@@ -36,6 +36,17 @@ Vocalist.create(
   ]
 )
 
+Composer.destroy_all
+Composer.create(
+  [
+    {name: "八王子P"},
+    {name: "ryo"},
+    {name: "otetsu"},
+    {name: "KulfiQ"},
+    {name: "Mitchie M"}
+  ]
+)
+
 # 曲データ
 Song.destroy_all
 Song.create(
@@ -45,28 +56,32 @@ Song.create(
       title_kana: "わーるどいずまいん",
       release_date: '2010-06-23',
       difficulty_ids: [1, 2, 3, 4, 5],
-      vocalist_ids: [1]
+      vocalist_ids: [1],
+      composer_id: 2,
     },
     {
       title: "星屑ユートピア",
       title_kana: "ほしくずゆーとぴあ",
       release_date: '2010-06-23',
       difficulty_ids: [1, 2, 3, 4, 5],
-      vocalist_ids: [4]
+      vocalist_ids: [4],
+      composer_id: 3
     },
     {
       title: "なりすましゲンガー",
       title_kana: "なりすましげんがー",
       release_date: '2016-1-28',
       difficulty_ids: [1, 2, 3, 4],
-      vocalist_ids: [1, 2]
+      vocalist_ids: [1, 2],
+      composer_id: 4
     },
     {
       title: "アゲアゲアゲイン",
       title_kana: "あげあげあげいん",
       release_date: '2016-2-26',
       difficulty_ids: [1, 2, 3, 4],
-      vocalist_ids: [1]
+      vocalist_ids: [1],
+      composer_id: 5
     }
   ]
 )
