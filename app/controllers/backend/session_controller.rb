335188b1328441d808af8ac67da36eea8f25fdbc
YutaGoto <goto@rejob.co.jp>
2016-03-13 18:04:08 +0900

    fix double render error

--- app/controllers/backend/session_controller.rb
+++ app/controllers/backend/session_controller.rb
@@ -1,14 +1,10 @@
 class Backend::SessionController < Backend::ApplicationController
   def sign_in
-    redirect_to backend_top_path if session[:id]
     if params.key?(:email) || params.key?(:password)
       admin = Admin.find_by email: params[:email]
-      if admin && admin.authenticate(params[:password])
+      if admin.present? && admin.authenticate(params[:password])
         session[:id] = admin.id
         redirect_to backend_top_path, notice: 'login'
-      else
-        session[:id] = nil
-        redirect_to backend_sign_in_path, notice: 'wrong'
       end
     end
   end
